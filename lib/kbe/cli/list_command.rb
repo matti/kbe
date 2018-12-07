05bd3f01a882078a4051141d8d8f53290c92645a
Matti Paksula <matti.paksula@iki.fi>
2018-12-07 19:38:08 +0200

    list pod containers

--- lib/kbe/cli/list_command.rb
+++ lib/kbe/cli/list_command.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
+require "json"
 
 module KBE
   module CLI
     class ListCommand < Clamp::Command
       option ['-h', '--help'], :flag, "help" do
         puts "kbe list"
+        puts "kbe list pod"
         exit 0
       end
 
+      parameter "[POD]", "pod"
+
       def execute
-        KBE.kubectl ["get pod"]
+        if pod
+          json = JSON.parse(`kubectl get pod #{pod} -o json`)
+          for container in json["spec"]["containers"]
+            puts container["name"]
+          end
+        else
+          KBE.kubectl ["get pod"]
+        end
       end
     end
   end
